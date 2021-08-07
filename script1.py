bb287febce34bc630a91ef6eff807df484d4a54d
Arnav Chavan <44640932+Arnav0400@users.noreply.github.com>
2021-08-07 12:58:00 +0530

    added imagenet script

--- script1.py
+++ script1.py
@@ -1,20 +1,10 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
-for model in ['rresnet_cifar_16_8']:
-    for dataset in ['CIFAR100']:
-        for wa in ['linear', 'swish']:
-            os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 100 --epochs 160 --save_weights --weight_activation {wa}')
+for model in ['resnet_18_1', 'resnet_18_2', 'resnet_cifar_18_4']:
+    for dataset in ['IMNET']:
+        os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 100 --epochs 90 --save_weights --datapath "/home/darya1/other_projects/Data/Imagenet" --gpus 4')
 
-for model in ['resnet_cifar_16_2', 'resnet_cifar_16_4', 'resnet_cifar_16_8']:
-    for dataset in ['CIFAR100']:
-        os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 100 --epochs 160 --save_weights')
-
-for model in ['rresnet_cifar_16_2', 'rresnet_cifar_16_4', 'rresnet_cifar_16_8']:
-    for dataset in ['CIFAR10']:
-        for wa in ['bireal', 'linear', 'swish']:
-            os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 10 --epochs 160 --save_weights --weight_activation {wa}')
-
-for model in ['resnet_cifar_16_1', 'resnet_cifar_16_2', 'resnet_cifar_16_4', 'resnet_cifar_16_8']:
-    for dataset in ['CIFAR10']:
-        os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 10 --epochs 160 --save_weights')
+for model in ['rresnet_cifar_18_2', 'rresnet_cifar_18_4']:
+    for dataset in ['IMNET']:
+        for wa in ['bireal']:
+            os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 10 --epochs 90 --save_weights --weight_activation {wa} --datapath "/home/darya1/other_projects/Data/Imagenet" --gpus 4')
