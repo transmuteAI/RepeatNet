abaa45021d7d1c8f57010ffef636edf8b9c44d53
Arnav Chavan <44640932+Arnav0400@users.noreply.github.com>
2021-08-27 23:34:25 +0530

    Delete script4.py

--- script4.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
-for model in [10]:
-    os.system(f'python main.py --model_name rep_vgg_{model} --dataset TINYIMNET --num_classes 200 --epochs 90 --weight_activation "linear"')
-
-for model in [5,6,8,10]:
-    os.system(f'python main.py --model_name rep_vgg_{model} --dataset TINYIMNET --num_classes 200 --epochs 90 --weight_activation "swish"')
