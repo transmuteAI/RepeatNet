5b40ea1eb7d8c95f33cfe81b23bd5bd6f6805533
Arnav Chavan <44640932+Arnav0400@users.noreply.github.com>
2021-08-27 23:34:17 +0530

    Delete script2.py

--- script2.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-for model in [4,5,7,8,9]:
-    os.system(f'python main.py --model_name VGG{model} --dataset CIFAR100 --num_classes 100 --epochs 160')
-
-for model in [4,8,9,10]:
-    os.system(f'python main.py --model_name VGG{model} --dataset CIFAR10 --num_classes 10 --epochs 160')
-
