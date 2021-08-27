b8846928c939e20ab5b17edb2e4032e95adee040
Arnav Chavan <44640932+Arnav0400@users.noreply.github.com>
2021-08-27 23:34:12 +0530

    Delete script1.py

--- script1.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-for model in ['resnet_cifar_16_1', 'resnet_cifar_16_4', 'resnet_cifar_16_8']:
-    for dataset in ['TINYIMNET']:
-        os.system(f'python main.py --model_name {model} --dataset {dataset} --num_classes 200 --epochs 90')
